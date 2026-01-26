324a3b082375f11e2efdfa48c78f3bf7d1e0f900
riadmurad22-ai <riadmurad22@gmail.com>
2026-01-26 20:47:34 +0700

    belum selesai coach - problem fetch data dari BE - React Vite

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://restaurant-be-400174736012.asia-southeast2.run.app",
+  baseURL:
+    "https://restaurant-be-400174736012.asia-southeast2.run.app/api-swagger/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default api;
+
+// https://restaurant-be-400174736012.asia-southeast2.run.app/api-swagger/
